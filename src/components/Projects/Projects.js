12dd8661e9623ecaae7fc06b09b14dcd4fb64d97
nileshrawatp1 <33788262+nileshrawatp1@users.noreply.github.com>
2024-03-12 14:47:44 +0530

    Projects added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,8 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import editor from "../../Assets/Projects/codeEditor.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import textconvertor from "../../Assets/Projects/textConversion.png";
+import newsApp from "../../Assets/Projects/newsApp.png";
+import codeEditor from "../../Assets/Projects/codeEditor.png";
 
 function Projects() {
   return (
@@ -14,34 +15,43 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          {/* Here are a few projects I've worked on recently. */}
-          Will be added Soon. 
+          Here are a few projects I've worked on recently.
         </p>
-        {/* <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={textconvertor}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/nileshrawatp1/Bits-0f-Bl0gs"
-              demoLink="https://blogs.nilesh-jit.tech/"
+              title="Text Convertor App"
+              description="Online text conversion app build with ReactJS only to help convert text from lower to uppar. Also includes github markdown table of conent. It help text in encoding and decoding aswell. Suports dark mode as well with easy to use UI."
+              ghLink="https://github.com/nileshrawatp1/textConversion"
+              demoLink="https://nilesh-text-conversion.vercel.app"              
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={newsApp}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/nileshrawatp1/online-editor.io"
-              demoLink="https://editor.nilesh-jit.tech/"              
+              title="Pocket-News"
+              description="My news app page build with React.js and Bootstrap Css which takes the news from news API and shows on the screen in defferent categories. The APi is just use for local server. So, can't make it live yet."
+              ghLink="https://github.com/nileshrawatp1/pocket-news"
             />
           </Col>
 
-        </Row> */}
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={codeEditor}
+              isBlog={false}
+              title="Code Conline"
+              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage."
+              ghLink="https://github.com/nileshrawatp1/Code-Editor"
+              demoLink="https://nilesh-code-editor.vercel.app"
+            />
+          </Col>
+
+        </Row> 
       </Container>
     </Container>
   );
